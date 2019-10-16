d5909ebdd801f5686929f659937c6d0a78a0668a
Harshith Gowda B T [C] <hgowdabt@infinera.com>
2019-10-16 14:48:00 +0530

    deleted test files and created new Folder structure

--- Kafka-to-Elasticsearch/src/test/java/com/infinera/dna/test/KafkatoElasticTest.java
+++ Kafka-to-Elasticsearch/src/test/java/com/infinera/dna/test/KafkatoElasticTest.java
@@ -1,4 +1,4 @@
-package com.techprimers.kafka.springbootkafkaconsumerexample;
+package com.infinera.dna.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootKafkaConsumerExampleApplicationTests {
+public class KafkatoElasticTest {
 
 	@Test
 	public void contextLoads() {
